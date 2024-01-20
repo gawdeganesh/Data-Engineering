64198bf6bfdc42d820fdfb265c719d219b114397
Ganesh Gawde <gawdeganesh92@gmail.com>
2024-01-19 19:11:20 -0600

    Removed specific files

--- Python_Practice/P1.py
+++
@@ -1,6 +0,0 @@
-print('Hello World')
-name = input('What is your name : \n')
-print(f'hello, {name}!!')
-
-
-
--- Python_Practice/P5.py
+++
@@ -1,5 +0,0 @@
-def printASCIITable():
-    for i in range(32,127):
-        print(i, chr(i))
-
-printASCIITable()
